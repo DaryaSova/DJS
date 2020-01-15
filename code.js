07fe77a90f89ef50d88378ffebdc5215c97cb644
DaryaSova <darya.sova.2016@mail.ru>
2020-01-16 02:09:26 +0400

    first commit

---
+++ code.js
@@ -0,0 +1,31 @@
+// let arr = ['plum.png', 'orande.jpg', 'apple.bmp'];
+
+// console.log(arr[1]);
+
+// // alert("Hello world")
+
+// // let answer = confirm("Are you here?");
+
+// // console.log(anser);
+
+// let answer = +prompt("Есть ли вам 18?" , "Да");  
+
+// console.log(typeof(answer)); // выводим то что ввел юзер
+
+// // console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+    
+
+console.log(incr++);
+console.log(decr--);
+
+console.log("3" == 2);
+console.log("2" === 2);
+
+let isCheked = true;
+let isClose = false;
+console.log(isCheked || isClose);
