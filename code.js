912f833aa45f18dd29a41741bf74d4dfbb36af6a
DaryaSova <darya.sova.2016@mail.ru>
2020-01-16 23:03:57 +0400

    mod script

--- code.js
+++ code.js
@@ -15,17 +15,65 @@
 // // console.log("arr" + " - object");
 // console.log(4 + +" - object");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
     
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log("3" == 2);
-console.log("2" === 2);
+// console.log("3" == 2);
+// console.log("2" === 2);
 
-let isCheked = true;
-let isClose = false;
-console.log(isCheked || isClose);
+// let isCheked = true;
+// let isClose = false;
+// console.log(isCheked || isClose);
+
+let num = 50;
+
+// if (num < 49) {
+//     console.log("Не верно");
+// } else if(num > 100) {
+//     console.log("МНого!");
+// } else {
+//     console.log("Верно!");
+// }
+
+// (num == 50) ? console.log("Верно!") : console.log("НЕверно!");
+
+// switch (num) {
+//     case num < 49: 
+//         console.log("Не верно");
+//         break;
+//     case num > 100:
+//         console.log("Много!");
+//         break;
+//     case num > 80 : 
+//         console.log("все еще много");
+//         break;
+//     case num == 50 : 
+//         console.log("Верно");
+//         break;    
+//     default : 
+//         console.log("что то пошлот не так");
+//         break;  
+// }   
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
