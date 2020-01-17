4bcc08761164a32f4c7bb91d0bfedd129b53df58
DaryaSova <darya.sova.2016@mail.ru>
2020-01-17 19:51:01 +0400

    циклы

--- code.js
+++ code.js
@@ -65,15 +65,15 @@ let num = 50;
 //     num++;
 // }
 
-do {
-    console.log(num);
-    num++;
-}
-while (num < 55);
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
