e7191007b2675213886952eba0dd4e3710a1c8f4
DaryaSova <darya.sova.2016@mail.ru>
2020-01-18 21:42:58 +0400

    object

--- code.js
+++ code.js
@@ -141,3 +141,28 @@
 // }
 
 // learnJS("Javasc", done);
+
+// let obj = new Object();
+
+let option = {
+    width : 1024,
+    height : 1024,
+    name : "test"
+};
+
+console.log(option.name);
+
+option.bool = false;
+option.colors = {
+    border : "black",
+    bg : "red"
+}
+delete option.bool;
+
+console.log(option);
+
+for (let key in option) {
+    console.log("Свойство " + key + " Значение " + option[key]);
+}
+
+console.log(Object.keys(option).length); 
