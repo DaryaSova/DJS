99aefad78d4c5b6eaf78a467c80cfc1af680a57f
DaryaSova <darya.sova.2016@mail.ru>
2020-01-20 06:43:27 +0400

    dom

--- code.js
+++ code.js
@@ -308,15 +308,36 @@
 // // // 3)
 // // console.log((typeof((!!"5"))));
 
-// let box = document.getElementById("box"),
-//     btn = document.getElementsByTagName("button"),
-//     circle = document.getElementsByClassName("circle"),
-//     heart = document.querySelectorAll(".wrapper .heart");
-//     oneHeart = document.querySelector(".heart");
-// console.log(box);
-// console.log(btn);
-// console.log(btn[0]);
-// console.log(circle[2]);
-// console.log(heart);
-// console.log(oneHeart);
+let box = document.getElementById("box"),
+    btn = document.getElementsByTagName("button"),
+    circle = document.getElementsByClassName("circle"),
+    heart = document.querySelectorAll(".heart");
+    oneHeart = document.querySelector(".heart");
+
+box.style.backgroundColor = "blue";
+
+btn[1].style.borderRadius = "100%";
+
+circle[0].style.backgroundColor = "red";
+circle[1].style.backgroundColor = "yellow";
+circle[2].style.backgroundColor = "green";
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = "blue";
+// }
+
+// heart.forEach(function(item, i , herats) {
+//     item.style.backgroundColor = "green";
+// }) ;
+
+
+let div = document.createElement("div"),
+    text = document.createTextNode("Тут был текст");
+    
+div .classList.add("black");
+
+document.body.appendChild(div);
+
+console.log(div);
+
 
